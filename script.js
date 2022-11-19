57271d6db9fefd118d5fbed84b50dfa9483ef806
Cristian <cprado416@gmail.com>
2022-11-18 22:06:47 -0800

    create rps buttons, make buttons run playRound function and pass correct values 'r,p,s'

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice () { // generates random computer selection 
 
     let choice = Math.floor(Math.random() * 3); 
 
@@ -12,58 +12,128 @@ function getComputerChoice () {
     
 }
 
-function playRound (playerSelection, computerSelection) {
 
-    // let computerInput = getComputerChoice();
-    // computerSelection = computerInput.toLowerCase();
 
-    // let playerInput = prompt(' Choose Rock , Paper , or Scissors...');
+function playRound (playerSelection, computerSelection) { 
+    // check for valid player response
+    while (playerSelection === null || playerSelection === ('')) {
 
+        alert('That\'s not a valid response... try again');
+        playerSelection = prompt('Choose Rock, Paper, or Scissors...');
+    }
+    // ----------------------------------------------------
+    // converts player response to lowercase
     playerSelection = playerSelection.toLowerCase();
     let result = '';
-    console.log(computerSelection);
-    console.log(playerSelection);
-
+    let counter = 0;
+    // ----------------------------------------------------
+    // values tracked by counter variable 
+    // win = 1
+    // loss = 2
+    // tie = 3
+    // ----------------------------------------------------
+    // determines if player wins by returning counter value 
     if (playerSelection === 'rock') { 
 
         computerSelection === 'paper' ? result = (`YOU LOSE! Paper beats Rock`) :
         computerSelection === 'scissors' ? result = ('YOU WIN! Rock beats Scissors') :
         result = ('TIE! Rock and Rock');
+        console.log(result);
+
+        result === (`YOU LOSE! Paper beats Rock`) ? counter = 2 :
+        result === ('YOU WIN! Rock beats Scissors') ? counter = 1 :
+        counter = 3;
+        return counter;
 
     } else if (playerSelection === 'paper') { 
 
         computerSelection === 'scissors' ? result = ('YOU LOSE! Scissors beat Paper') :
         computerSelection === 'rock' ? result = ('YOU WIN! Paper beats Rock') :
         result = ('TIE! Paper and Paper');
+        console.log(result);
+
+        result === ('YOU LOSE! Scissors beat Paper') ? counter = 2 :
+        result === ('YOU WIN! Paper beats Rock') ? counter = 1 :
+        counter = 3;
+        return counter;
 
     } else if (playerSelection === ('scissors')) { // Scissors
 
         computerSelection === 'rock' ? result = ('YOU LOSE! Rock beats Scissors') :
         computerSelection === 'paper' ? result = ('YOU WIN! Scissors beat Paper') :
         result = ('TIE! Scissors and Scissors');
+        console.log(result);
+
+        result === ('YOU LOSE! Rock beats Scissors') ? counter = 2 :
+        result === ('YOU WIN! Scissors beat Paper') ? counter = 1 :
+        counter = 3;
+        return counter;
 
     } else {
-
-        result = 'Thats not a valid entry....';
-
+        alert('That is not an option... neither side gets points...');
     }
 
-    return result;
-}
+} // end playRound --------------------------------------------------------------
 
-const playerSelection = "Paper";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// ROCK button code 
+
+const rockBtn = document.querySelector('#ROCK');
+
+rockBtn.addEventListener('click', () => {
+    playRound('rock',getComputerChoice());
+})
+
+// PAPER button code 
+
+const paperBtn = document.querySelector('#PAPER');
+
+paperBtn.addEventListener('click', () => {
+    playRound('paper',getComputerChoice());
+})
+
+
+// SCISSORS button code 
+
+const scissorsBtn = document.querySelector('#SCISSORS');
+
+scissorsBtn.addEventListener('click',() =>{
+    playRound('scissors', getComputerChoice());
+})
+
+// function game() {
+
+//     let compScore = 0;
+
+//     let playerScore = 0;
+
+//     for (i = 0; i < 5; i++) {
+
+//         let playerSelection = prompt('Choose Rock, Paper, or Scissors...');
+
+//         let computerSelection = getComputerChoice();
+
+//         let counter = playRound(playerSelection,computerSelection);
+
+//     // win = 1
+//     // loss = 2
+
+//         counter === 1 ? playerScore += 1 :
+//         counter === 2 ? compScore += 1 :
+//         playerScore += 0;
+        
+//         alert(`Your Score: ${playerScore} Computer Score : ${compScore}`);
+
+
+//     }
+
+//     playerScore > compScore ? alert(`YOU WON THE GAME!! Final Score YOU: ${playerScore} COMPUTER: ${compScore}`) :
+//     playerScore < compScore ? alert(`YOU LOSE THE GAME! Final Score YOU: ${playerScore} COMPUTER: ${compScore}`) :
+//     alert ('THE GAME ENDED IN A DRAW!');
+// }
 
 
 
+//keep score by checking if comp or player won
 
+// display winner at the end by comparing scores
 
-// rock - scissors 
-// rock < paper 
-
-// scissors > paper
-// scissors < rock
-
-// paper > scissors
-// paper < rock 
